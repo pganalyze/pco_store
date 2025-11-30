56b41ce7d7e201510de7f126e7f436900514a3f1
Sean Linsley <code@seanlinsley.com>
2025-11-30 12:43:14 -0500

    Add store_grouped function

--- src/lib.rs
+++ src/lib.rs
@@ -313,7 +313,7 @@ pub fn store(args: TokenStream, item: TokenStream) -> TokenStream {
                 Ok(results)
             }
 
-            /// Writes the provided data to disk.
+            /// Writes the data to disk.
             pub async fn store(db: &deadpool_postgres::Object, rows: Vec<#name>) -> anyhow::Result<()> {
                 if rows.is_empty() {
                     return Ok(());
@@ -321,6 +321,39 @@ pub fn store(args: TokenStream, item: TokenStream) -> TokenStream {
                 let mut grouped_rows: ahash::AHashMap<_, Vec<#name>> = ahash::AHashMap::new();
                 for row in rows {
                     grouped_rows.entry((#store_group)).or_default().push(row);
+                }
+                let sql = #store_sql;
+                let types = &[#store_types];
+                let stmt = db.copy_in(&db.prepare_cached(&sql).await?).await?;
+                let writer = tokio_postgres::binary_copy::BinaryCopyInWriter::new(stmt, types);
+                futures::pin_mut!(writer);
+                for rows in grouped_rows.into_values() {
+                    #timestamp_collect
+                    writer.as_mut().write(&[#store_values]).await?;
+                }
+                writer.finish().await?;
+                Ok(())
+            }
+
+            /// Writes the data to disk, with the provided grouping closure applied.
+            ///
+            /// This can be used to improve the compression ratio and reduce read IO, for example
+            /// by compacting real-time data into a single row per hour / day / week.
+            pub async fn store_grouped<F, R>(
+                db: &deadpool_postgres::Object,
+                rows: Vec<#name>,
+                grouping: F,
+            ) -> anyhow::Result<()>
+            where
+                F: Fn(&#name) -> R,
+                R: Eq + std::hash::Hash,
+            {
+                if rows.is_empty() {
+                    return Ok(());
+                }
+                let mut grouped_rows: std::collections::HashMap<_, Vec<#name>> = std::collections::HashMap::new();
+                for row in rows {
+                    grouped_rows.entry((#store_group grouping(&row))).or_default().push(row);
                 }
                 let sql = #store_sql;
                 let types = &[#store_types];
